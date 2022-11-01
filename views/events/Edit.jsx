59d7f2324f484eb8429b34e5c9cdb4acec2c5b15
MichaelOb89 <michaelobrien.t@hotmail.com>
2022-10-31 22:19:13 -0400

    Fix date inputs

--- views/events/Edit.jsx
+++ views/events/Edit.jsx
@@ -15,7 +15,7 @@ class Edit extends React.Component {
                 <h1>Edit {event.title}</h1>    
                 <form method='POST' action={`/events/${event._id}?_method=PUT`}>
                     Title: <input type="text" name='title' defaultValue={event.title}/><br />
-                    Date: <input type="date" name='date' value={`${event.date.getUTCFullYear()}-${month}-${event.date.getDate()}`}/><br />
+                    Date: <input type="datetime-local" name='date' value={`${event.date.getUTCFullYear()}-${month}-${event.date.getDate()}`}/><br />
                     Distance{`(miles)`}: <input type="number" name='distance' step="0.1" defaultValue={event.distance}/><br />
                     <input type="submit" value='Submit Activity'/>
                 </form>
--- views/events/New.jsx
+++ views/events/New.jsx
@@ -8,7 +8,7 @@ class New extends React.Component{
                 <h1>Create new Event</h1>
                 <form method='POST' action='/events'>
                     Title: <input type="text" name='title'/><br />
-                    Date: <input type="date" name='date'/><br />
+                    Date: <input type="datetime-local" name='date'/><br />
                     Distance(Miles): <input type="number" step="0.1" name='distance'/><br />
                     <input type="submit" value='Submit Activity'/>
                 </form>
